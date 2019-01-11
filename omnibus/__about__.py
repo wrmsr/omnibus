2e93761a08d5918361f478f16a645e323c5e9ce8
wrmsr <timwilloney@gmail.com>
2019-01-11 15:16:25 -0800

    --

--- omnibus/__about__.py
+++ omnibus/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'omnibus'
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 __description__ = ''
 __author__ = 'wrmsr'
 __url__ = 'https://github.com/wrmsr/omnibus'
