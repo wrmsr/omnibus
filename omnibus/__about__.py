8fa7f609c7b4dbf743bc6336a8fbbdd01cae5dae
wrmsr <timwilloney@gmail.com>
2019-04-06 18:48:12 -0700

    --

--- omnibus/__about__.py
+++ omnibus/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'omnibus'
-__version__ = '0.0.33'
+__version__ = '0.0.34'
 __description__ = ''
 __author__ = 'wrmsr'
 __url__ = 'https://github.com/wrmsr/omnibus'
--- omnibus/lang.py
+++ omnibus/lang.py
@@ -1234,4 +1234,16 @@ def await_futures(
     return False
 
 
+class ImmediateExecutor(concurrent.futures.Executor):
+
+    def submit(self, fn, *args, **kwargs):
+        future = concurrent.futures.Future()
+        try:
+            result = fn(*args, **kwargs)
+            future.set_result(result)
+        except Exception as e:
+            future.set_exception(e)
+        return future
+
+
 # endregion
