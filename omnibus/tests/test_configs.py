8a836ce5cbba7bb2d05825ddad36e4a1bfa12668
wrmsr <timwilloney@gmail.com>
2020-03-05 15:49:09 -0800

    --

--- omnibus/tests/test_configs.py
+++ omnibus/tests/test_configs.py
@@ -36,35 +36,68 @@ def flatten(unflattened: StrMap) -> StrMap:
 class UnflattenNode(lang.Abstract, ta.Generic[K]):
 
     @lang.abstract
-    def get(self) -> ta.Any:
+    def get(self, key: K) -> ta.Any:
         raise NotImplementedError
 
     @lang.abstract
     def put(self, key: K, value: ta.Any) -> None:
         raise NotImplementedError
 
-    """
-        default <V> V setDefault(K key, Supplier<V> supplier)
-        {
-            V ret = (V) get(key);
-            if (ret == null) {
-                ret = supplier.get();
-                put(key, ret);
-            }
-            return ret;
-        }
-
-        Object build();
-
-        static Object build(Object value)
-        {
-            checkNotNull(value);
-            return value instanceof UnflattenNode ? ((UnflattenNode) value).build() : value;
-        }
-    """
-
     def setdefault(self, key: K, supplier: ta.Callable[[], V]) -> V:
-        ret = self.get()
+        ret = self.get(key)
+        if ret is NOT_SET:
+            ret = supplier()
+            self.put(key, ret)
+        return ret
+
+    @lang.abstract
+    def build(self) -> ta.Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def maybe_build(value: ta.Any) -> ta.Any:
+        check.not_none(value)
+        return value.build() if isinstance(value, UnflattenNode) else value
+
+
+class UnflattenDict(UnflattenNode[str]):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._dict = {}
+
+    def get(self, key: str) -> ta.Any:
+        return self._dict.get(key, NOT_SET)
+
+    def put(self, key: str, value: ta.Any) -> None:
+        check.arg(key not in self._dict)
+        self._dict[key] = value
+
+    def build(self) -> ta.Any:
+        return {k: UnflattenNode.maybe_build(v) for k, v in self._dict.items()}
+
+
+class UnflattenList(UnflattenNode[int]):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._list = []
+
+    def get(self, key: int) -> ta.Any:
+        check.arg(key >= 0)
+        return self._list[key] if key <= len(self._list) else NOT_SET
+
+    def put(self, key: int, value: ta.Any) -> None:
+        check.arg(key >= 0)
+        if key >= len(self._list):
+            self._list.extend([NOT_SET] * (key - len(self._list) + 1))
+        check.arg(self._list[key] is NOT_SET)
+        self._list[key] = value
+
+    def build(self) -> ta.Any:
+        return [UnflattenNode.maybe_build(e) for e in self._list]
 
 
 def unflatten(flattened: StrMap) -> StrMap:
