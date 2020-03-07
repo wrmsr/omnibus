d0ac18dcde266f6ff2463f2734d8b1cd716644a6
wrmsr <timwilloney@gmail.com>
2020-03-06 19:47:17 -0800

    --

--- omnibus/tests/test_sql.py
+++ omnibus/tests/test_sql.py
@@ -1,7 +1,19 @@
+import pytest
 import sqlalchemy as sa
 
+from .. import docker
 from .. import sql
 
 
 def test_pymysql():
     assert sql.pymysql_render_statement(sa.select([1])) == 'SELECT 1'
+
+
+@pytest.mark.xfail()
+def test_docker_mysql():
+    with docker.client_context() as client:
+        eps = docker.get_container_tcp_endpoints(
+            client,
+            [('docker_omnibus-mysql-master_1', 3306)])
+
+    print(eps)
