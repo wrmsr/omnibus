b95351d5a5505e2c72f67b5b1d71624131ba9d1c
wrmsr <timwilloney@gmail.com>
2020-03-10 00:12:00 -0700

    --

--- omnibus/dispatch.py
+++ omnibus/dispatch.py
@@ -454,8 +454,9 @@ class _RegistryClassMeta(abc.ABCMeta):
                     self._regs[key] = value
 
             else:
-                reg.register(value, cls_dct=self._dict)
-                self._dict[f'__{hex(id(value))}'] = value
+                if isinstance(reg, RegistryProperty):
+                    reg.register(value, cls_dct=self._dict)
+                    self._dict[f'__{hex(id(value))}'] = value
 
         def __delitem__(self, key):
             del self._dict[key]
