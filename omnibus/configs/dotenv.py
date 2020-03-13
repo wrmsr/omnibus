cbc2d59c4907075b8f6f4dae98c3300c8b6890c9
wrmsr <timwilloney@gmail.com>
2020-03-12 19:51:04 -0700

    --

--- omnibus/configs/dotenv.py
+++ omnibus/configs/dotenv.py
@@ -1,7 +1,10 @@
 """
 https://github.com/kojiromike/parameter_expansion/blob/master/pe.py
 """
+import re
 import typing as ta
+
+from .. import check
 
 
 def dumb_parse_lines(lines: ta.Iterable[str]) -> ta.Iterable[ta.Tuple[str, str]]:
@@ -16,6 +19,21 @@ def dumb_parse_lines(lines: ta.Iterable[str]) -> ta.Iterable[ta.Tuple[str, str]]
         k = k.strip()
         v = v.strip().strip("'").strip('"')
         yield k, v
+
+
+class Parser:
+
+    def __init__(self, buf: str) -> None:
+        super().__init__()
+
+        self._buf = check.isinstance(buf, str)
+        self._pos = 0
+
+    def read_pattern(self, pat: ta.Pattern) -> ta.Match:
+        match = check.not_none(pat.match(self._buf, self._pos))
+        check.state(match.end() >= self._pos)
+        self._pos = match.end()
+        return match
 
 
 def parse(buf: str) -> ta.Iterable[ta.Tuple[str, ta.Optional[str]]]:
--- omnibus/configs/tests/test_configs.py
+++ omnibus/configs/tests/test_configs.py
@@ -1,6 +1,10 @@
-from .. import flattening as flattening_
+import re
+import textwrap
+
 from .. import configs as configs_
+from .. import dotenv as dotenv_
 from .. import fields as fields_
+from .. import flattening as flattening_
 
 
 def test_flattening():
@@ -65,3 +69,18 @@ def test_configs():
         comment='a comment',
     )
     assert db_config.url == 'a url'
+
+
+def test_dotenv():
+    parser = dotenv_.Parser(textwrap.dedent("""
+    a=b
+    c=d
+    """))
+
+    parser.read_pattern(re.compile(r'\s*'))
+    m0 = parser.read_pattern(re.compile(r'\w+=\w+'))
+    parser.read_pattern(re.compile(r'\s*'))
+    m1 = parser.read_pattern(re.compile(r'\w+=\w+'))
+
+    assert m0 is not None
+    assert m1 is not None
