4f05f34cc6f4000dbd5123d0e873f5e294cf5f78
wrmsr <timwilloney@gmail.com>
2020-03-13 17:24:10 -0700

    --

--- omnibus/tests/test_lifecycle2.py
+++ omnibus/tests/test_lifecycle2.py
@@ -2,4 +2,14 @@ from .. import lifecycle2
 
 
 def test_lifecycle2():
-    print(lifecycle2.LifecycleStates.CONSTRUCTED)
+    class L(lifecycle2.Lifecycle):
+        pass
+
+    l = L()
+
+    c = lifecycle2.LifecycleController(l)
+
+    c.lifecycle_construct()
+    c.lifecycle_start()
+    c.lifecycle_stop()
+    c.lifecycle_destroy()
