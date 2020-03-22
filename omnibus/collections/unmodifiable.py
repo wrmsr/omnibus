6a76c32805c297764d29786131c8b35e9c3da9ab
wrmsr <timwilloney@gmail.com>
2020-03-22 16:50:02 -0700

    --

--- omnibus/collections/unmodifiable.py
+++ omnibus/collections/unmodifiable.py
@@ -1,10 +1,33 @@
 import typing as ta
 
+from .. import check
+
 
 T = ta.TypeVar('T')
 K = ta.TypeVar('K')
 V = ta.TypeVar('V')
 
 
+class UnmodifiableSequence(ta.Sequence[T]):
+
+    def __init__(self, target: ta.Sequence[T]) -> None:
+        super().__init__()
+
+        self._target = check.not_none(target)
+
+
+class UnmodifiableSet(ta.AbstractSet[T]):
+
+    def __init__(self, target: ta.AbstractSet[T]) -> None:
+        super().__init__()
+
+        self._target = check.not_none(target)
+
+
 class UnmodifiableMapping(ta.Mapping[K, V]):
-    pass
+
+    def __init__(self, target: ta.Mapping[K, V]) -> None:
+        super().__init__()
+
+        self._target = check.not_none(target)
+
