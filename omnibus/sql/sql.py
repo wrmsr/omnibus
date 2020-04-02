6180a0005bf38322aa171cc0c00643dde20e888f
wrmsr <timwilloney@gmail.com>
2020-04-02 01:10:32 -0700

    --

--- omnibus/sql/sql.py
+++ omnibus/sql/sql.py
@@ -2,6 +2,14 @@
 TODO:
  - pools
   - disco
+  - sets need refill capability
+  - devolve to just recycling queues?
+  - not totally, need aurora punch
+  - multiple fallback tiers
+  - inline (or at least ‘wake the bg thread’) table poll / forced refresh
+  - master, 0% but warm
+  - lb endpoint
+  - writer endpoint
  - engines, connsets
  - instrumentation
  - state?
