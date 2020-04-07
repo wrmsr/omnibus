b75fb62b5563ad1c9c20840c290ce9e7e0538257
wrmsr <timwilloney@gmail.com>
2020-04-07 11:48:39 -0700

    --

--- omnibus/dataclasses/build2.py
+++ omnibus/dataclasses/build2.py
@@ -35,6 +35,9 @@ class ClassProcessor(ta.Generic[TypeT]):
         self._cls = check.isinstance(cls, type)
         self._params = check.isinstance(params, dc._DataclassParams)
 
+        if self._params.order and not self._params.eq:
+            raise ValueError('eq must be true if order is true')
+
     def _set_new_attribute(self, name: str, value: ta.Any) -> bool:
         if name in self._cls.__dict__:
             return True
@@ -103,6 +106,9 @@ class ClassProcessor(ta.Generic[TypeT]):
 
         return fields
 
+    def _install_fields(self) -> None:
+        setattr(self._cls, dc._FIELDS, self._fields)
+
     @properties.cached
     def _field_maps_by_field_type(self) -> ta.Mapping[str, ta.Sequence[dc.Field]]:
         ret = {}
@@ -114,75 +120,98 @@ class ClassProcessor(ta.Generic[TypeT]):
     def _instance_fields(self) -> ta.List[dc.Field]:
         return list(self._field_maps_by_field_type.get(dc._FIELD, {}).values())
 
-    def __call__(self) -> TypeT:
-        setattr(self._cls, dc._PARAMS, self._params)
+    def _install_init(self) -> None:
+        # Does this class have a post-init function?
+        has_post_init = hasattr(self._cls, dc._POST_INIT_NAME)
 
-        self._check_frozen_bases()
-
-        setattr(self._cls, dc._FIELDS, self._fields)
-
-        if self._params.init:
-            # Does this class have a post-init function?
-            has_post_init = hasattr(self._cls, dc._POST_INIT_NAME)
-
-            # Include InitVars and regular fields (so, not ClassVars).
-            flds = [f for f in self._fields.values() if f._field_type in (dc._FIELD, dc._FIELD_INITVAR)]
-            self._set_new_attribute(
-                '__init__',
-                dc._init_fn(
-                    flds,
-                    self._params.frozen,
-                    has_post_init,
-                    '__dataclass_self__' if 'self' in self._fields
-                    else 'self',
-                    self._globals,
-                )
+        # Include InitVars and regular fields (so, not ClassVars).
+        flds = [f for f in self._fields.values() if f._field_type in (dc._FIELD, dc._FIELD_INITVAR)]
+        self._set_new_attribute(
+            '__init__',
+            dc._init_fn(
+                flds,
+                self._params.frozen,
+                has_post_init,
+                '__dataclass_self__' if 'self' in self._fields
+                else 'self',
+                self._globals,
             )
+        )
 
+    def _install_repr(self) -> None:
+        flds = [f for f in self._instance_fields if f.repr]
+        self._set_new_attribute('__repr__', dc._repr_fn(flds, self._globals))
+
+    def _install_eq(self) -> None:
+        flds = [f for f in self._instance_fields if f.compare]
+        self_tuple = dc._tuple_str('self', flds)
+        other_tuple = dc._tuple_str('other', flds)
+        self._set_new_attribute('__eq__', dc._cmp_fn('__eq__', '==', self_tuple, other_tuple, globals=self._globals))
+
+    def _install_order(self) -> None:
+        flds = [f for f in self._instance_fields if f.compare]
+        self_tuple = dc._tuple_str('self', flds)
+        other_tuple = dc._tuple_str('other', flds)
+        for name, op in [
+            ('__lt__', '<'),
+            ('__le__', '<='),
+            ('__gt__', '>'),
+            ('__ge__', '>='),
+        ]:
+            if self._set_new_attribute(name, dc._cmp_fn(name, op, self_tuple, other_tuple, globals=self._globals)):
+                raise TypeError(
+                    f'Cannot overwrite attribute {name} in class {self._cls.__name__}. Consider using functools.total_ordering')
+
+    def _install_frozen(self) -> None:
+        for fn in dc._frozen_get_del_attr(self._cls, self._instance_fields, self._globals):
+            if self._set_new_attribute(fn.__name__, fn):
+                raise TypeError(f'Cannot overwrite attribute {fn.__name__} in class {self._cls.__name__}')
+
+    def _maybe_install_doc(self) -> None:
+        if not getattr(self._cls, '__doc__'):
+            self._cls.__doc__ = (self._cls.__name__ + str(inspect.signature(self._cls)).replace(' -> None', ''))
+
+    def _maybe_install_hash(self) -> bool:
         # Was this class defined with an explicit __hash__?  Note that if __eq__ is defined in this class, then python
         # will automatically set __hash__ to None.  This is a heuristic, as it's possible that such a __hash__ == None
         # was not auto-generated, but it close enough.
         class_hash = self._cls.__dict__.get('__hash__', dc.MISSING)
         has_explicit_hash = not (class_hash is dc.MISSING or (class_hash is None and '__eq__' in self._cls.__dict__))
-
-        # If we're generating ordering methods, we must be generating the eq methods.
-        if self._params.order and not self._params.eq:
-            raise ValueError('eq must be true if order is true')
-
-        if self._params.repr:
-            flds = [f for f in self._instance_fields if f.repr]
-            self._set_new_attribute('__repr__', dc._repr_fn(flds, self._globals))
-
-        if self._params.eq:
-            flds = [f for f in self._instance_fields if f.compare]
-            self_tuple = dc._tuple_str('self', flds)
-            other_tuple = dc._tuple_str('other', flds)
-            self._set_new_attribute('__eq__', dc._cmp_fn('__eq__', '==', self_tuple, other_tuple, globals=self._globals))
-
-        if self._params.order:
-            flds = [f for f in self._instance_fields if f.compare]
-            self_tuple = dc._tuple_str('self', flds)
-            other_tuple = dc._tuple_str('other', flds)
-            for name, op in [
-                ('__lt__', '<'),
-                ('__le__', '<='),
-                ('__gt__', '>'),
-                ('__ge__', '>='),
-            ]:
-                if self._set_new_attribute(name, dc._cmp_fn(name, op, self_tuple, other_tuple, globals=self._globals)):
-                    raise TypeError(
-                        f'Cannot overwrite attribute {name} in class {self._cls.__name__}. Consider using functools.total_ordering')
-
-        if self._params.frozen:
-            for fn in dc._frozen_get_del_attr(self._cls, self._instance_fields, self._globals):
-                if self._set_new_attribute(fn.__name__, fn):
-                    raise TypeError(f'Cannot overwrite attribute {fn.__name__} in class {self._cls.__name__}')
-
-        hash_action = dc._hash_action[bool(self._params.unsafe_hash), bool(self._params.eq), bool(self._params.frozen), has_explicit_hash]
+        hash_action = dc._hash_action[(
+            bool(self._params.unsafe_hash),
+            bool(self._params.eq),
+            bool(self._params.frozen),
+            has_explicit_hash,
+        )]
         if hash_action:
             self._cls.__hash__ = hash_action(self._cls, self._instance_fields, self._globals)
+            return True
+        else:
+            return False
 
-        if not getattr(self._cls, '__doc__'):
-            self._cls.__doc__ = (self._cls.__name__ + str(inspect.signature(self._cls)).replace(' -> None', ''))
+    def __call__(self) -> TypeT:
+        setattr(self._cls, dc._PARAMS, self._params)
+
+        self._check_frozen_bases()
+
+        self._install_fields()
+
+        if self._params.init:
+            self._install_init()
+
+        if self._params.repr:
+            self._install_repr()
+
+        if self._params.eq:
+            self._install_eq()
+
+        if self._params.order:
+            self._install_order()
+
+        if self._params.frozen:
+            self._install_frozen()
+
+        self._maybe_install_hash()
+        self._maybe_install_doc()
 
         return self._cls
