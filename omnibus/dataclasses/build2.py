01ffebf673c2a2546769f6acbaaeedc4ae565031
wrmsr <timwilloney@gmail.com>
2020-04-07 11:42:30 -0700

    --

--- omnibus/dataclasses/build2.py
+++ omnibus/dataclasses/build2.py
@@ -4,6 +4,8 @@ import sys
 import typing as ta
 
 from .. import check
+from .. import lang
+from .. import properties
 
 
 TypeT = ta.TypeVar('TypeT', bound=type, covariant=True)
@@ -39,27 +41,33 @@ class ClassProcessor(ta.Generic[TypeT]):
         setattr(self._cls, name, value)
         return False
 
-    def __call__(self) -> TypeT:
-        fields = {}
-
+    @properties.cached
+    def _globals(self) -> ta.MutableMapping[str, ta.Any]:
         if self._cls.__module__ in sys.modules:
-            globals = sys.modules[self._cls.__module__].__dict__
+            return sys.modules[self._cls.__module__].__dict__
         else:
-            globals = {}
+            return {}
 
-        setattr(self._cls, dc._PARAMS, self._params)
+    @properties.cached
+    def _dataclass_rmro(self) -> ta.List[type]:
+        return [b for b in self._cls.__mro__[-1:0:-1] if getattr(b, dc._FIELDS, None)]
 
-        any_frozen_base = False
-        has_dataclass_bases = False
-        for b in self._cls.__mro__[-1:0:-1]:
-            # Only process classes that have been processed by our decorator.  That is, they have a _FIELDS attribute.
+    def _check_frozen_bases(self) -> None:
+        any_frozen_base = any(getattr(b, dc._PARAMS).frozen for b in self._dataclass_rmro)
+        if any_frozen_base:
+            if any_frozen_base and not self._params.frozen:
+                raise TypeError('cannot inherit non-frozen dataclass from a frozen one')
+            if not any_frozen_base and self._params.frozen:
+                raise TypeError('cannot inherit frozen dataclass from a non-frozen one')
+
+    @properties.cached
+    def _fields(self) -> ta.Mapping[str, dc.Field]:
+        fields = {}
+        for b in self._dataclass_rmro:
             base_fields = getattr(b, dc._FIELDS, None)
             if base_fields:
-                has_dataclass_bases = True
                 for f in base_fields.values():
                     fields[f.name] = f
-                if getattr(b, dc._PARAMS).frozen:
-                    any_frozen_base = True
 
         # Annotations that are defined in this class (not in base classes).  If __annotations__ isn't present, then this
         # class adds no new annotations.  We use this to compute fields that are added by this class.
@@ -93,18 +101,43 @@ class ClassProcessor(ta.Generic[TypeT]):
             if isinstance(value, dc.Field) and not name in cls_annotations:
                 raise TypeError(f'{name!r} is a field but has no type annotation')
 
-        # Check rules that apply if we are derived from any dataclasses.
-        if has_dataclass_bases:
-            # Raise an exception if any of our bases are frozen, but we're not.
-            if any_frozen_base and not self._params.frozen:
-                raise TypeError('cannot inherit non-frozen dataclass from a frozen one')
+        return fields
 
-            # Raise an exception if we're frozen, but none of our bases are.
-            if not any_frozen_base and self._params.frozen:
-                raise TypeError('cannot inherit frozen dataclass from a non-frozen one')
+    @properties.cached
+    def _field_maps_by_field_type(self) -> ta.Mapping[str, ta.Sequence[dc.Field]]:
+        ret = {}
+        for f in self._fields.values():
+            ret.setdefault(f._field_type, {})[f.name] = f
+        return ret
 
-        # Remember all of the fields on our class (including bases).  This also marks this class as being a dataclass.
-        setattr(self._cls, dc._FIELDS, fields)
+    @properties.cached
+    def _instance_fields(self) -> ta.List[dc.Field]:
+        return list(self._field_maps_by_field_type.get(dc._FIELD, {}).values())
+
+    def __call__(self) -> TypeT:
+        setattr(self._cls, dc._PARAMS, self._params)
+
+        self._check_frozen_bases()
+
+        setattr(self._cls, dc._FIELDS, self._fields)
+
+        if self._params.init:
+            # Does this class have a post-init function?
+            has_post_init = hasattr(self._cls, dc._POST_INIT_NAME)
+
+            # Include InitVars and regular fields (so, not ClassVars).
+            flds = [f for f in self._fields.values() if f._field_type in (dc._FIELD, dc._FIELD_INITVAR)]
+            self._set_new_attribute(
+                '__init__',
+                dc._init_fn(
+                    flds,
+                    self._params.frozen,
+                    has_post_init,
+                    '__dataclass_self__' if 'self' in self._fields
+                    else 'self',
+                    self._globals,
+                )
+            )
 
         # Was this class defined with an explicit __hash__?  Note that if __eq__ is defined in this class, then python
         # will automatically set __hash__ to None.  This is a heuristic, as it's possible that such a __hash__ == None
@@ -116,38 +149,18 @@ class ClassProcessor(ta.Generic[TypeT]):
         if self._params.order and not self._params.eq:
             raise ValueError('eq must be true if order is true')
 
-        if self._params.init:
-            # Does this class have a post-init function?
-            has_post_init = hasattr(self._cls, dc._POST_INIT_NAME)
-
-            # Include InitVars and regular fields (so, not ClassVars).
-            flds = [f for f in fields.values() if f._field_type in (dc._FIELD, dc._FIELD_INITVAR)]
-            self._set_new_attribute(
-                '__init__',
-                dc._init_fn(
-                    flds,
-                    self._params.frozen,
-                    has_post_init,
-                    '__dataclass_self__' if 'self' in fields
-                    else 'self',
-                    globals,
-                )
-            )
-
-        field_list = [f for f in fields.values() if f._field_type is dc._FIELD]
-
         if self._params.repr:
-            flds = [f for f in field_list if f.repr]
-            self._set_new_attribute('__repr__', dc._repr_fn(flds, globals))
+            flds = [f for f in self._instance_fields if f.repr]
+            self._set_new_attribute('__repr__', dc._repr_fn(flds, self._globals))
 
         if self._params.eq:
-            flds = [f for f in field_list if f.compare]
+            flds = [f for f in self._instance_fields if f.compare]
             self_tuple = dc._tuple_str('self', flds)
             other_tuple = dc._tuple_str('other', flds)
-            self._set_new_attribute('__eq__', dc._cmp_fn('__eq__', '==', self_tuple, other_tuple, globals=globals))
+            self._set_new_attribute('__eq__', dc._cmp_fn('__eq__', '==', self_tuple, other_tuple, globals=self._globals))
 
         if self._params.order:
-            flds = [f for f in field_list if f.compare]
+            flds = [f for f in self._instance_fields if f.compare]
             self_tuple = dc._tuple_str('self', flds)
             other_tuple = dc._tuple_str('other', flds)
             for name, op in [
@@ -156,18 +169,18 @@ class ClassProcessor(ta.Generic[TypeT]):
                 ('__gt__', '>'),
                 ('__ge__', '>='),
             ]:
-                if self._set_new_attribute(name, dc._cmp_fn(name, op, self_tuple, other_tuple, globals=globals)):
+                if self._set_new_attribute(name, dc._cmp_fn(name, op, self_tuple, other_tuple, globals=self._globals)):
                     raise TypeError(
                         f'Cannot overwrite attribute {name} in class {self._cls.__name__}. Consider using functools.total_ordering')
 
         if self._params.frozen:
-            for fn in dc._frozen_get_del_attr(self._cls, field_list, globals):
+            for fn in dc._frozen_get_del_attr(self._cls, self._instance_fields, self._globals):
                 if self._set_new_attribute(fn.__name__, fn):
                     raise TypeError(f'Cannot overwrite attribute {fn.__name__} in class {self._cls.__name__}')
 
         hash_action = dc._hash_action[bool(self._params.unsafe_hash), bool(self._params.eq), bool(self._params.frozen), has_explicit_hash]
         if hash_action:
-            self._cls.__hash__ = hash_action(self._cls, field_list, globals)
+            self._cls.__hash__ = hash_action(self._cls, self._instance_fields, self._globals)
 
         if not getattr(self._cls, '__doc__'):
             self._cls.__doc__ = (self._cls.__name__ + str(inspect.signature(self._cls)).replace(' -> None', ''))
