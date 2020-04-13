bffda88db1bc53a43dcdac523c6538da6e159dd2
wrmsr <timwilloney@gmail.com>
2020-04-12 23:45:23 -0700

    --

---
+++ omnibus/dev/dev.py
@@ -0,0 +1,25 @@
+"""
+TODO:
+ - tpch
+ - mypy
+ - https://github.com/lyft/pynamodb-mypy/tree/master/pynamodb_mypy
+ - https://sobolevn.me/2019/08/testing-mypy-types
+ - project gens (cy/rs/cc)
+ - dev helping fuse stuff
+ - venv helpers
+ - makefile shit
+ - dockerfiles
+ - cmake - https://gist.github.com/wrmsr/b36337aaf2fda33faf092454a106fda6 + FT gen_cmake stuff
+ - cargo stuff
+ - pycharm interop (but runtime pydevd still in core)
+ - gh shit
+ - protobuf shit
+ - type hierarchy graphviz
+ - tame freeze (pyinstaller? pyoxidizer?)
+ - pex
+ - pyenv stuff
+ - ‘semi-static’ checks..? match exhaustiveness etc
+  - DO NOT WANT compile-timme forced impl-all mandate but do want flake
+ - https://github.com/github/semantic
+ - d3js webservy stuff using own webservers
+"""
