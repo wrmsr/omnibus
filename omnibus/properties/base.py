7776f31dd261bd0dd313375c3e2d65517499866c
wrmsr <timwilloney@gmail.com>
2020-04-14 09:40:12 -0700

    --

--- omnibus/properties/base.py
+++ omnibus/properties/base.py
@@ -1,3 +1,7 @@
+"""
+TODO:
+ - https://github.com/JetBrains/intellij-community/blob/8a0b1c9e911750d74a3be4cfee746804c674f27f/python/python-psi-impl/src/com/jetbrains/python/psi/impl/PyClassImpl.java#L703  # noqa
+"""
 import typing as ta
 
 
