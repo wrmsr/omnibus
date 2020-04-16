fc0d300ee3d4c0cf3833222138a01ae12d270c7d
wrmsr <timwilloney@gmail.com>
2020-04-16 15:46:15 -0700

    --

--- omnibus/configs/dotenv.py
+++ omnibus/configs/dotenv.py
@@ -6,6 +6,54 @@ import typing as ta
 from .. import check
 
 
+"""
+_ESCAPE_DECODER = codecs.getdecoder('unicode_escape')
+_POSIX_VARIABLE = re.compile('\$\{[^\}]*\}')
+
+
+def decode_escaped(escaped):
+    return _ESCAPE_DECODER(escaped)[0]
+
+
+def parse_line(line):
+    line = line.strip()
+
+    # Ignore lines with `#` or which doesn't have `=` in it.
+    if not line or line.startswith('#') or '=' not in line:
+        return None, None
+
+    k, v = line.split('=', 1)
+
+    if k.startswith('export '):
+        (_, _, k) = k.partition('export ')
+
+    # Remove any leading and trailing spaces in key, value
+    k, v = k.strip(), v.strip()
+
+    if v:
+        v = v.encode('unicode-escape').decode('ascii')
+        quoted = v[0] == v[-1] in ['"', "'"]
+        if quoted:
+            v = decode_escaped(v[1:-1])
+
+    return k, v
+
+
+def resolve_nested_variables(values):
+    def replacement(name):
+        ret = os.getenv(name, values.get(name, ""))
+        return ret
+
+    def re_sub_callback(match_object):
+        return replacement(match_object.group()[2:-1])
+
+    for k, v in values.items():
+        values[k] = _POSIX_VARIABLE.sub(re_sub_callback, v)
+
+    return values
+"""
+
+
 def dumb_parse_lines(lines: ta.Iterable[str]) -> ta.Iterable[ta.Tuple[str, str]]:
     for line in lines:
         line = line.strip()
