9b4770fa6d9bc38254a9b69451f9e2cfde6ca350
wrmsr <timwilloney@gmail.com>
2020-04-16 21:41:27 -0700

    --

--- omnibus/instruments/__init__.py
+++ omnibus/instruments/__init__.py
@@ -1,4 +1,8 @@
 from .. import lang
+from .metrics import CompositeMetricsCollection  # noqa
+from .metrics import MetricsCollection  # noqa
+from .metrics import NopMetricsCollection  # noqa
+from .metrics import PrefixedMetricsCollection  # noqa
 
 
 lang.warn_unstable()
