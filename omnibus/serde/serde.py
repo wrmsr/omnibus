368743c03caa83e0ecdf738610ba5e2100de4e5f
wrmsr <timwilloney@gmail.com>
2020-04-17 18:18:05 -0700

    --

--- omnibus/serde/serde.py
+++ omnibus/serde/serde.py
@@ -49,6 +49,57 @@ https://github.com/FasterXML/jackson-databind/wiki/Mapper-Features
 subtype reg:
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 @JsonSubTypes({@JsonSubTypes.Type(value = OutputNode.class, name = "output"), ...
+
+objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+objectMapper.disable(DeserializationFeature.ACCEPT_FLOAT_AS_INT);
+objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+objectMapper.setDefaultPropertyInclusion(JsonInclude.Value.construct(JsonInclude.Include.NON_ABSENT, JsonInclude.Include.ALWAYS));
+objectMapper.disable(MapperFeature.AUTO_DETECT_CREATORS);
+objectMapper.disable(MapperFeature.AUTO_DETECT_FIELDS);
+objectMapper.disable(MapperFeature.AUTO_DETECT_SETTERS);
+objectMapper.disable(MapperFeature.AUTO_DETECT_GETTERS);
+objectMapper.disable(MapperFeature.AUTO_DETECT_IS_GETTERS);
+objectMapper.disable(MapperFeature.USE_GETTERS_AS_SETTERS);
+objectMapper.disable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS);
+objectMapper.disable(MapperFeature.INFER_PROPERTY_MUTATORS);
+objectMapper.disable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS);
+
+public static final Set<MapperFeature> DEFAULT_DISABLED_FEATURES = ImmutableSet.copyOf(new MapperFeature[] {
+        MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS,
+        MapperFeature.AUTO_DETECT_CREATORS,
+        MapperFeature.AUTO_DETECT_FIELDS,
+        MapperFeature.AUTO_DETECT_GETTERS,
+        MapperFeature.AUTO_DETECT_IS_GETTERS,
+        MapperFeature.AUTO_DETECT_SETTERS,
+        MapperFeature.INFER_CREATOR_FROM_CONSTRUCTOR_PROPERTIES,
+        MapperFeature.INFER_PROPERTY_MUTATORS,
+        MapperFeature.USE_BASE_TYPE_AS_DEFAULT_IMPL,
+        MapperFeature.USE_GETTERS_AS_SETTERS,
+});
+
+public static final List<Supplier<Module>> DEFAULT_MODULE_FACTORIES = new CopyOnWriteArrayList<>(ImmutableList.of(
+        GuavaModule::new,
+        JavaTimeModule::new,
+        Jdk8Module::new
+));
+
+public static ObjectMapper configureObjectMapper(ObjectMapper objectMapper)
+{
+    objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    DEFAULT_DISABLED_FEATURES.forEach(objectMapper::disable);
+    DEFAULT_MODULE_FACTORIES.forEach(f -> objectMapper.registerModule(f.get()));
+    getAfterburnerModuleFactory().ifPresent(f -> objectMapper.registerModule(f.get()));
+    return objectMapper;
+}
+
+public static final Set<JsonParser.Feature> RELAXED_JSON_PARSER_ENABLED_FEATURES = ImmutableSet.of(
+        JsonParser.Feature.ALLOW_COMMENTS,
+        JsonParser.Feature.ALLOW_YAML_COMMENTS,
+        JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES,
+        JsonParser.Feature.ALLOW_SINGLE_QUOTES
+);
 """
 import collections.abc
 import datetime
