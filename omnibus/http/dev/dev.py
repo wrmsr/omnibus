fdcc05cada0e8152389437c04eeed622c1de0271
wrmsr <timwilloney@gmail.com>
2020-05-02 22:21:00 -0700

    --

---
+++ omnibus/http/dev/dev.py
@@ -0,0 +1,4 @@
+"""
+TODO:
+ - dev app (debugger)
+"""
--- omnibus/retries.py
+++ omnibus/retries.py
@@ -1,6 +1,9 @@
 """
 TODO:
  - coordination (redis? coord iface?)
+ - dogpile
+ - mv faults
+
 https://engineering.shopify.com/blogs/engineering/circuit-breaker-misconfigured
 """
 from . import lang
