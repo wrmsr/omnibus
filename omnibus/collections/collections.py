5aa9cadb71dc96d48235d34c353c84ca0a6a9490
wrmsr <timwilloney@gmail.com>
2020-06-15 19:26:05 -0700

    --

--- omnibus/collections/collections.py
+++ omnibus/collections/collections.py
@@ -51,13 +51,13 @@ def toposort(data: ta.Dict[T, ta.Set[T]]) -> ta.Iterator[ta.Set[T]]:
     for k, v in data.items():
         v.discard(k)
     extra_items_in_deps = functools.reduce(set.union, data.values()) - set(data.keys())
-    data.update(dict((item, set()) for item in extra_items_in_deps))
+    data.update({item: set() for item in extra_items_in_deps})
     while True:
         ordered = set(item for item, dep in data.items() if not dep)
         if not ordered:
             break
         yield ordered
-        data = dict((item, (dep - ordered)) for item, dep in data.items() if item not in ordered)
+        data = {item: (dep - ordered) for item, dep in data.items() if item not in ordered}
     if data:
         raise ValueError('Cyclic dependencies exist among these items: ' + ' '.join(repr(x) for x in data.items()))
 
