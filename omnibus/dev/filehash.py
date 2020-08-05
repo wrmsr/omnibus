cb803203ff02945008211aee44278d6ee8810847
wrmsr <timwilloney@gmail.com>
2020-08-05 13:32:15 -0700

    --

--- omnibus/dev/filehash.py
+++ omnibus/dev/filehash.py
@@ -1,13 +1,16 @@
 import os.path
 import subprocess
+import time
 
 from .. import argparse as ap
+from .. import check
 from .. import dataclasses as dc
 from .. import json
 
 
 class Entry(dc.Pure):
     name: str
+    htime: float
     size: int
     mtime: float
     md5: str
@@ -29,19 +32,33 @@ class Cli(ap.Cli):
     )
     def gen(self) -> None:
         dp = os.path.abspath(os.getcwd())
+        ebn = {}
         for fn in sorted(os.listdir(dp)):
             fp = os.path.abspath(os.path.join(dp, fn))
             if not os.path.isfile(fp):
                 continue
+            htime = time.time()
             bmd5 = subprocess.check_output(['md5', '-q', fp])
             md5 = bmd5.decode('utf-8').strip()
             entry = Entry(
                 name=fn,
+                htime=htime,
                 size=os.path.getsize(fp),
                 mtime=os.path.getmtime(fp),
                 md5=md5,
             )
-            print(entry)
+            check.not_in(entry.name, ebn)
+            ebn[entry.name] = entry
+
+        dct = {
+            e.name: {
+                k: v
+                for k, v in dc.asdict(e).items()
+                if k != 'name'
+            }
+            for e in sorted(ebn.values(), key=lambda e: e.name)
+        }
+        print(json.dumps(dct, indent=True))
 
 
 def main():
