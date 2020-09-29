05c16ffcdd436715d1c3cd9b0b6ca5f96a54d10a
wrmsr <timwilloney@gmail.com>
2020-09-29 10:16:02 -0700

    --

--- setup.py
+++ setup.py
@@ -346,7 +346,6 @@ class Distribution(distutils.core.Distribution):
 
 
 if __name__ == '__main__':
-    breakpoint()
     sys.stderr.write(repr(sys.argv) + '\n')
     setuptools.setup(
         name=ABOUT['__title__'],
