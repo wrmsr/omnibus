239a28815c1edefdc6c396c73b3f575dc2dda4dd
wrmsr <timwilloney@gmail.com>
2020-10-25 13:48:27 -0700

    --

--- a.py
+++
@@ -1,9 +0,0 @@
-import builtins as b
-
-
-class C(object):
-    a = b.TypeError
-    def a(self, x: b.OSError) -> None:
-        pass
-    def a(self, x: b.NotImplementedError) -> None:
-        pass
