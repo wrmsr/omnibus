f50f48d0d8acaca06ba023b7de80dd2c71a48ceb
wrmsr <timwilloney@gmail.com>
2020-10-25 14:37:12 -0700

    --

--- omnibus/expect/spawn/pty.py
+++ omnibus/expect/spawn/pty.py
@@ -11,3 +11,12 @@ class PtySpawn(BaseSpawn, lang.Abstract):
             command: ta.Sequence[str],
     ) -> None:
         super().__init__(command)
+
+    def write(self, buf: bytes) -> int:
+        raise NotImplementedError
+
+    def write_eof(self) -> None:
+        raise NotImplementedError
+
+    def read_nb(self, size: int, timeout: ta.Union[int, float, None] = None) -> bytes:
+        raise NotImplementedError
