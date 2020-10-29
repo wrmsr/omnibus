db3c99cb67e00c3c7ab5995037c84ea0810ec000
wrmsr <timwilloney@gmail.com>
2020-10-29 11:33:56 -0700

    --

--- omnibus/proto/proto.py
+++ omnibus/proto/proto.py
@@ -7,6 +7,8 @@ TODO:
  - **grpc**
   - get example proto
 
+https://github.com/yihuang/cprotobuf/blob/master/cprotobuf/internal.pyx
+https://github.com/appnexus/pyrobuf/blob/master/pyrobuf/protobuf/templates/proto_pxd.tmpl
 https://blog.envoyproxy.io/dynamic-extensibility-and-protocol-buffers-dcd0bf0b8801
 https://developers.google.com/protocol-buffers/docs/encoding
 https://developers.google.com/protocol-buffers/docs/reference/cpp/google.protobuf.text_format
