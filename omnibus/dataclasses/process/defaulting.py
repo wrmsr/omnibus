b7274d82ee266e3555a6ac6b705d701ca36a5d2c
wrmsr <timwilloney@gmail.com>
2021-01-31 16:50:31 -0800

    --

--- omnibus/dataclasses/process/defaulting.py
+++ omnibus/dataclasses/process/defaulting.py
@@ -48,17 +48,18 @@ class Defaulting(Aspect):
         return [Fields]
 
     def check(self) -> None:
-        # Make sure we don't have fields without defaults following fields with defaults.  This actually would be caught
-        # when exec-ing the function source code, but catching it here gives a better error message, and future-proofs
-        # us in case we build up the function using ast.
-        seen_default = False
-        for f in self.ctx.spec.fields.init:
-            # Only consider fields in the __init__ call.
-            if f.init:
-                if not (f.default is dc.MISSING and f.default_factory is dc.MISSING):
-                    seen_default = True
-                elif seen_default:
-                    raise TypeError(f'non-default argument {f.name!r} follows default argument')
+        if not self.ctx.spec.extra_params.kwonly:
+            # Make sure we don't have fields without defaults following fields with defaults.  This actually would be
+            # caught when exec-ing the function source code, but catching it here gives a better error message, and
+            # future-proofs us in case we build up the function using ast.
+            seen_default = False
+            for f in self.ctx.spec.fields.init:
+                # Only consider fields in the __init__ call.
+                if f.init:
+                    if not (f.default is dc.MISSING and f.default_factory is dc.MISSING):
+                        seen_default = True
+                    elif seen_default:
+                        raise TypeError(f'non-default argument {f.name!r} follows default argument')
 
     @properties.cached
     def deriver_nodes(self) -> ta.Sequence[DeriverNode]:
