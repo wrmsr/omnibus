2c3c9934dea845063a89372f87d898707af48d13
wrmsr <timwilloney@gmail.com>
2021-02-22 10:17:56 -0800

    --

---
+++ omnibus/serde/formats/__init__.py
@@ -0,0 +1,10 @@
+"""
+TODO:
+ - in-house yaml :/
+ - formalize json5/minml
+ - stream json
+ - fast (enough) json? ditch uj/orj?
+ - edn r/w
+ - cbor
+ - ** no proto / thrift ** - dedicated pkgs
+"""
